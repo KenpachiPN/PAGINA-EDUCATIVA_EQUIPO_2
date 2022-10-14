2a1e2cdcdeb3767f80a9d4d4c84eb974457681e1
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-10-14 10:24:44 -0500

    VersionEstable

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+/*================SHOW MENU#####################*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-/*============================= CHANGE BACKGRAUND HEADER=============*/
-function scrollHeader(){
-    const header=document.getElementById('header')
-    //when  the scroll is greater than 50 vieport height,add scroll-header class to the header tag
-    if(this.scrolly>=50)header.classList.add('scroll-header');else header.classList.remove('scroll-header')
+
+/*########### menu show################*/
+/*validste if const exist*/
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
 }
-window.addEventListener('scroll',scrollHeader)
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*============================= CHANGE BACKGRAUND HEADER=============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    //when  the scroll is greater than 50 vieport height,add scroll-header class to the header tag
+    if (this.scrolly >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
