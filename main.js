97a9484be1d6b8d2a6e52b2567a02c7b98ca1f7d
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-10-20 11:02:50 -0500

    VersiónEstableParte1

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ if (navClose) {
 function scrollHeader() {
     const header = document.getElementById('header')
     //when  the scroll is greater than 50 vieport height,add scroll-header class to the header tag
-    if (this.scrolly >= 50) header.classList.add('scroll-header');
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); 
     else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
